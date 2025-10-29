dcfd76406b92364fede5d42b0c734e3c29f3f38a
Isaac-Monted <isaacmonted10@gmail.com>
2025-10-29 13:30:57 -0600

    Agregar controladores y vistas para las categorías 'carnicos', 'mantequilla' y 'venancio', y actualizar lógica en scripts.js para manejar nuevas rutas.

---
+++ controllers/CarnicosController.php
@@ -0,0 +1,12 @@
+<?php
+    class CarnicosController{
+       
+        public function index(){
+        /*   require_once 'views/loggin/index.php';  */
+         require_once 'views/carnicos/index.php';
+         
+        }
+
+        
+        
+    }
---
+++ controllers/MantequillaController.php
@@ -0,0 +1,12 @@
+<?php
+    class MantequillaController{
+       
+        public function index(){
+        /*   require_once 'views/loggin/index.php';  */
+         require_once 'views/mantequilla/index.php';
+         
+        }
+
+        
+        
+    }
