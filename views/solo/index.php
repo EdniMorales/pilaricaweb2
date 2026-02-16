a04966d76dc5fb9fef13a31a53ca133c4febf05a
EdniMorales <rimora.29@gmail.com>
2026-02-16 16:31:32 -0600

    cambios-nosotros

--- views/solo/index.php
+++ views/solo/index.php
@@ -10,45 +10,45 @@
                     </div> -->
                 </div>
             </header>
-             <section class="fondo-desc-1 py-5 px-5">
+
+
+        <section class="fondo-desc-1 py-5 px-5">
                 <div class=""><!-- px-5 py-5 lead fw-normal container -->
                     <p class="info-somos">En Lácteos La Pilarica, elaboramos quesos artesanales desde 1980, manteniendo vivo un compromiso genuino con la calidad y el sabor auténtico.</p>
                 </div>
-             </section>
-            <section class="fondo-desc-historia-1 py-5 " id="scroll-target">
-                <div class=" container fondo-desc-historia-2 px-5 py-5">
-                <div class=" px-5 my-5 title-n-history">
-                    <h1 class="title-n-history text-center mb-5">Nuestra Historia</h1>
-                    <div class="row gx-5 align-items-center">              
-                    <div class="col-lg-6 text-center">
-                        <img class="img-fluid rounded mb-5 mb-lg-0 text-center" src="<?= base_url ?>assets/nosotros/fotocollage.png" alt="..." /></div>     
-                        <div class="col-lg-6">
-                            <p class="fw-bolder title2-n-history ">El comienzo de nuestro sueño</p>
-                            <p class="lead fw-normal descrip-n-history">La Pilarica nació en 1980, gracias al esfuerzo y la pasión del señor Fidel Humberto Alfaro Sotres. Con mucho cariño, comenzó a elaborar queso en pequeños recipientes, experimentando hasta encontrar la fórmula perfecta. Así fue como, paso a paso, dio vida al sabor auténtico que hoy nos representa.
-                            </p>
-                            <p class="fw-bolder title3-n-history">Nada supera la tradición de hacer buen queso</p>
-                            <p class="lead fw-normal descrip-n-history">Con el aumento de la demanda, crecieron también nuestras instalaciones y el equipo de trabajo. Poco a poco, ampliamos nuestra variedad de productos: de elaborar solo queso Oaxaca y Panela, pasamos a crear también Manchego, Ranchero, Cotija, Enchilado y más.
-                            </p>
-                            <h2 class="fw-bolder title4-n-history">Actualidad</h2>
-                            <p class="lead fw-normal descrip-n-history">Actulamente también se producen postres y yogurt, productos que han dado a la empresa un mejor lugar en el mercado. </p>
-                        </div>                 
-                     </div>
-                </div>
-        </div>
+        </section>
+
+
+             
+            <section class="fondo-desc-historia-1 py-5" id="scroll-target">
+                        <div class="container fondo-desc-historia-2 px-5 py-5">
+                            <div class=" px-5 my-5 title-n-history">
+                                <h1 class="title-n-history text-center mb-5">Nuestra Historia</h1>
+                                <div class="row gx-5 align-items-center">              
+                                <div class="col-lg-6 text-center">
+                                    <img class="img-fluid rounded mb-5 mb-lg-0 text-center" src="<?= base_url ?>assets/nosotros/fotocollage.png" alt="..." /></div>     
+                                    <div class="col-lg-6">
+                                        <p class="fw-bolder title2-n-history ">El comienzo de nuestro sueño</p>
+                                        <p class="lead fw-normal descrip-n-history">La Pilarica nació en 1980, gracias al esfuerzo y la pasión del señor Fidel Humberto Alfaro Sotres. Con mucho cariño, comenzó a elaborar queso en pequeños recipientes, experimentando hasta encontrar la fórmula perfecta. Así fue como, paso a paso, dio vida al sabor auténtico que hoy nos representa.
+                                        </p>
+                                        <p class="fw-bolder title3-n-history">Nada supera la tradición de hacer buen queso</p>
+                                        <p class="lead fw-normal descrip-n-history">Con el aumento de la demanda, crecieron también nuestras instalaciones y el equipo de trabajo. Poco a poco, ampliamos nuestra variedad de productos: de elaborar solo queso Oaxaca y Panela, pasamos a crear también Manchego, Ranchero, Cotija, Enchilado y más.
+                                        </p>
+                                        <h2 class="fw-bolder title4-n-history">Actualidad</h2>
+                                        <p class="lead fw-normal descrip-n-history">Actulamente también se producen postres y yogurt, productos que han dado a la empresa un mejor lugar en el mercado. </p>
+                                    </div>                 
+                                </div>
+                            </div>
+                        </div>
             </section>
-            <section class="fondo-mision py-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6">
-                    <p class="info-tit fw-bolder ">Nuestra Misión</p>
-                    <p class="info-somos-1 lead fw-normal container">Alimentar a la sociedad con productos auténticos, naturales, funcionales, accesibles
-                    e innovadores, guiados por la pasión y el amor por lo que hacemos, con un firme compromiso en satisfacer y superar las expectativas de nuestros clientes y consumidores.
-                    </p>
-                </div>
-                     <div class="col-lg-6 ">
-                        <img src="<?= base_url ?>assets/nosotros/foto-mision.jpg" alt="" class="info-somos-2">
-                    </div>
-                </div>
-            </section>
+
+
+
+
+
+
+
+   
             <section class="fondo py-5">
                 <div class="px-5 py-5">
                   <p class="info-tit fw-bolder dancing-script-bold">Nuestra Visión</p>
@@ -57,6 +57,7 @@
                     </p>
                 </div>
             </section>
+
             <section class="fondo py-5">
                 <div class="px-5 py-5">
                   <p class="info-tit fw-bolder dancing-script-bold">Nuestros Valores</p>
@@ -64,6 +65,8 @@
                     </p>
                 </div>
             </section>
+
+
        <!--      <section class="fondo py-5 text-center sect-1">
                 <div class="fondo px-5 my-5">
                     <div class="row">
@@ -102,11 +105,61 @@
             </section> -->
          <!--    <section class="masthead6 fondo">
             </section> -->
-            <section class="fondo  text-center">
+            <section class="fondo text-center">
                 <div class="fondo px-5 py-5">
                   <p class="info-tit-ce fw-bolder dancing-script-bold">Estamos celebrando 45 años</p>
                     <p class="info-tit-ce lead fw-normal container dancing-script-bold">   De sabor y calidad
                     </p>
                 </div>
             </section>
+
+
+    <section class="fondo-mision py-5">              
+        <div class="row col-lg">
+        <p class="info-tit">Nuestra Misión</p>
+            <p class="info-somos-1">Alimentar a la sociedad con productos auténticos, naturales, funcionales, accesibles e innovadores, guiados por la pasión y el amor por lo que hacemos, con un firme compromiso en satisfacer y superar las expectativas de nuestros clientes y consumidores.
+            </p>
+        </div>
+        <div class="col-lg">
+            <img src="<?= base_url ?>assets/nosotros/quesos-maduros.jpg" alt="" class="info-somos-2">
+        </div>
         
+    </section>
+
+    <section>
+                             <div class=" px-5">
+                    <div class="row justify-content-center">
+                       <div class="col-lg-12 col-xxl-6">
+                            <div class="text-center my-5">
+                                     <p class="info-tit">Nuestra Misión</p>
+            <p class="info-somos-1">Alimentar a la sociedad con productos auténticos, naturales, funcionales, accesibles e innovadores, guiados por la pasión y el amor por lo que hacemos, con un firme compromiso en satisfacer y superar las expectativas de nuestros clientes y consumidores.
+            </p>
+                              <!--   <p class="fw-bolder mb-3 title-love-1">Quienes</p>    
+                                <p class="fw-bolder mb-3 title-love-2">somos?</p> -->
+                            </div>
+                        </div>
+
+
+
+          <div class="col-lg-12 col-xxl-6">
+                            <div class="text-center my-5">
+                                     <p class="info-tit">Nuestra Misión</p>
+            <p class="info-somos-1">Alimentar a la sociedad con productos auténticos, naturales, funcionales, accesibles e innovadores, guiados por la pasión y el amor por lo que hacemos, con un firme compromiso en satisfacer y superar las expectativas de nuestros clientes y consumidores.
+            </p>
+                              <!--   <p class="fw-bolder mb-3 title-love-1">Quienes</p>    
+                                <p class="fw-bolder mb-3 title-love-2">somos?</p> -->
+                            </div>
+                        </div>
+
+
+
+
+
+
+                    </div> 
+                </div>
+
+
+
+
+    </section>
